8e64402f39a7a98e5226ce1a86861c5366ac1592
Vishwa <vishwanatha.setty01@gmail.com>
2021-11-13 18:49:52 +0530

    Sec Demo

---
+++ src/main/java/com/demo/vishwa/second_demo_project/App.java
@@ -0,0 +1,22 @@
+package com.demo.vishwa.second_demo_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Welcome to Java World...." );
+    }
+}
+
+//public class App
+//{
+//	public static void main( String[] args )
+//	{
+//		System.out.println( "Hellow vishwa ! This si your first Typed Java class")
+//	}
+//	
+//}
